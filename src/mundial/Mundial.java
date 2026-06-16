a610becb1cc8c9a4393340cbdf7a15838627d0e9
Marco Ramirez Quiros <maaramirezqu@est.utn.ac.cr>
2026-06-16 16:29:47 -0600

    Clase Mundial
    Se creo la clase Mundial

---
+++ src/mundial/Mundial.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mundial;
+
+/**
+ *
+ * @author mr117
+ */
+public class Mundial {
+}
