07d35e1fd3bdb65c0f52ad9bde421b6a08e78838
carolelectrica <josedanieldelgadoquiros@gmail.com>
2026-06-19 14:55:28 -0600

    JUgador
    se modfico la clase jugar anadiendole sus atributos con su herencia de persona

--- src/jugador/Jugador.java
+++ src/jugador/Jugador.java
@@ -3,11 +3,41 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package jugador;
-
+import java.time.LocalDate;
+import persona.Persona;
+        
 /**
  *
  * @author Student
  */
-public class Jugador {
+public class Jugador extends Persona {
+    protected int numero;
+    protected String posicion;
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public String getPosicion() {
+        return posicion;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public void setPosicion(String posicion) {
+        this.posicion = posicion;
+    }
+
+    public Jugador(int numero, String posicion, String nombre, String nacionalidad, LocalDate fechaNacimiento) {
+        super(nombre, nacionalidad, fechaNacimiento);
+        this.numero = numero;
+        this.posicion = posicion;
+    }
+@Override
+    public String toString() {
+        return "Jugador: " + nombre + ", Nacionalidad: " + nacionalidad + ", Edad: " + getEdad() + " años" +", Número: " + numero + ", Posición: " + posicion;
+  
     
 }
