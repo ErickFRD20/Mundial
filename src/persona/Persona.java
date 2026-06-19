d87d46b3296e597c75649cb115c12f387a26cdc9
ErickFRD <erreyesde@est.utn.ac.cr>
2026-06-19 14:41:53 -0600

    Persona
    Se completa la clase persona con sus atributos ya guardados

--- src/persona/Persona.java
+++ src/persona/Persona.java
@@ -3,11 +3,50 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package persona;
-
+import java.time.LocalDate;
+import java.time.Period;
 /**
  *
  * @author Student
  */
 public class Persona {
+    protected String nombre;
+    protected String nacionalidad;
+    protected LocalDate fechaNacimiento;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getNacionalidad() {
+        return nacionalidad;
+    }
+
+    public LocalDate getFechaNacimiento() {
+        return fechaNacimiento;
+    }
     
+    public int getEdad(){
+        return Period.between(fechaNacimiento,LocalDate.now()).getYears();
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setNacionalidad(String nacionalidad) {
+        this.nacionalidad = nacionalidad;
+    }
+
+    public void setFechaNacimiento(LocalDate fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public Persona(String nombre, String nacionalidad, LocalDate fechaNacimiento) {
+        this.nombre = nombre;
+        this.nacionalidad = nacionalidad;
+        this.fechaNacimiento = fechaNacimiento;
+    }
+    
+   
 }
