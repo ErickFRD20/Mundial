8a7694b68085bd99865207b2bfcf3039dbc24501
Dylan Gutiérrez <dylangutierrezctdf@gmail.com>
2026-06-19 15:58:40 -0600

    Se crea la clase partido
    Se creo la clase partido y se añadio todo lo necesario para que funcione el codigo.

--- src/partido/Partido.java
+++ src/partido/Partido.java
@@ -3,11 +3,127 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package partido;
+import java.time.LocalDate;
+import seleccion.Seleccion;
+import arbitro.Arbitro;
+import java.time.LocalTime;
 
 /**
  *
  * @author Student
  */
 public class Partido {
+    private Seleccion seleccionLocal;
+    private Seleccion seleccionVisitante;
+    private String estadio;
+    private LocalDate fecha;
+    private LocalTime hora;
+    private Arbitro arbitro;
+    private String resultado;
+    
+    public Partido(){}
+    public Partido(Seleccion seleccionLocal, Seleccion seleccionVisitante,String estadio,LocalDate fecha, LocalTime hora, Arbitro arbitro, String resultado){
+        this.seleccionLocal = seleccionLocal;
+        this.seleccionVisitante = seleccionVisitante;
+        this.estadio = estadio;
+        this.fecha = fecha;
+        this.hora = hora;
+        this.arbitro = arbitro;
+        this.resultado = resultado;
+    }
+    public Seleccion getSeleccionLocal() {
+
+        return seleccionLocal;
+
+    }
+
+    public void setSeleccionLocal(Seleccion seleccionLocal) {
+
+        this.seleccionLocal = seleccionLocal;
+
+    }
+
+    public Seleccion getSeleccionVisitante() {
+
+        return seleccionVisitante;
+
+    }
+
+    public void setSeleccionVisitante(Seleccion seleccionVisitante) {
+
+        this.seleccionVisitante = seleccionVisitante;
+
+    }
+
+    public String getEstadio() {
+
+        return estadio;
+
+    }
+
+    public void setEstadio(String estadio) {
+
+        this.estadio = estadio;
+
+    }
+
+    public LocalDate getFecha() {
+
+        return fecha;
+
+    }
+
+    public void setFecha(LocalDate fecha) {
+
+        this.fecha = fecha;
+
+    }
+
+    public LocalTime getHora() {
+
+        return hora;
+
+    }
+
+    public void setHora(LocalTime hora) {
+
+        this.hora = hora;
+
+    }
+
+    public Arbitro getArbitro() {
+
+        return arbitro;
+
+    }
+
+    public void setArbitro(Arbitro arbitro) {
+
+        this.arbitro = arbitro;
+
+    }
+
+    public String getResultado() {
+
+        return resultado;
+
+    }
+
+    public void setResultado(String resultado) {
+
+        this.resultado = resultado;
+    }
+       @Override
+    public String toString() {
+        return "Partido{" +
+                "seleccionLocal=" + seleccionLocal +
+                "seleccionVisitante=" + seleccionVisitante +
+                "estadio='" + estadio + '\'' +
+                "fecha=" + fecha +
+                "hora=" + hora +
+                "arbitro=" + arbitro +
+                "resultado='" + resultado + '\'' +
+                '}';
+    }
     
 }
